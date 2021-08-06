e714953c90a6cf2551f01940dd27b1bdf038d5cb
Lars Yencken <lars@yencken.org>
2021-08-06 15:28:52 +0200

    Improve audit script to cover all urls

--- owid/walden/audit.py
+++ owid/walden/audit.py
@@ -4,8 +4,10 @@
 #  audit.py
 #  walden
 #
+
 import click
 import jsonschema
+import requests
 
 from owid.walden import catalog
 
@@ -18,9 +20,23 @@ def audit() -> None:
     i = 0
     for document in catalog.iter_docs():
         jsonschema.validate(document, schema)
+        check_url(document["source_data_url"])
+        if "owid_data_url" in document:
+            check_url(document["owid_data_url"])
         i += 1
 
-    print(f"{i} catalog entries ok")
+    print(f"{i} catalog entries ok, all urls ok")
+
+
+def check_url(url: str) -> None:
+    "Make sure the URL is still valid."
+    resp = requests.head(url)
+    if resp.status_code != 200:
+        raise InvalidOrExpiredUrl(url)
+
+
+class InvalidOrExpiredUrl(Exception):
+    pass
 
 
 if __name__ == "__main__":
