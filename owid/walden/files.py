f74f1dad618f757f0afa6c34874f6dd5fe371364
Marigold <mojmir.vinkler@gmail.com>
2022-04-04 10:37:04 +0200

    feature: Show progress bar for large file downloads

--- owid/walden/files.py
+++ owid/walden/files.py
@@ -8,28 +8,59 @@ import tempfile
 import hashlib
 import json
 
+import math
 from os import path, walk
 from shutil import move  # noqa; re-exported for convenience
-from typing import Iterator, Optional, Tuple
+from typing import Iterator, Optional, Tuple, IO
+from rich.progress import track
 
 import requests
 
 from .ui import log
 
 
+def _stream_to_file(
+    r: requests.Response,
+    file: IO[bytes],
+    chunk_size: int = 2**14,
+    progress_bar_min_bytes: int = 2**30,
+) -> str:
+    """Stream the response to the file, returning the checksum.
+    :param progress_bar_min_bytes: Minimum number of bytes to display a progress bar for.
+    """
+    # check header to get content length, in bytes
+    total_length = int(r.headers["content-length"])
+
+    md5 = hashlib.md5()
+
+    streamer = r.iter_content(chunk_size=chunk_size)
+
+    if total_length > progress_bar_min_bytes:
+        streamer = track(  # type: ignore
+            streamer,
+            description="Downloading",
+            total=math.ceil(total_length / chunk_size),
+        )
+
+    for chunk in streamer:  # 16k
+        file.write(chunk)
+        md5.update(chunk)
+
+    return md5.hexdigest()
+
+
 def download(
     url: str, filename: str, expected_md5: Optional[str] = None, quiet: bool = False
 ) -> None:
     "Download the file at the URL to the given local filename."
-    md5 = hashlib.md5()
     with tempfile.NamedTemporaryFile(mode="wb", delete=False) as temp, requests.get(
         url, stream=True
     ) as r:
         r.raise_for_status()
-        for chunk in r.iter_content(chunk_size=2**14):  # 16k
-            temp.write(chunk)
-            md5.update(chunk)
-        if expected_md5 and md5.hexdigest() != expected_md5:
+
+        md5 = _stream_to_file(r, temp)
+
+        if expected_md5 and md5 != expected_md5:
             raise ChecksumDoesNotMatch(f"for file downloaded from {url}")
         move(temp.name, filename)
     if not quiet:
