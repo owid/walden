045609bf225c84c12c5c49cac17905f9741dab46
Pablo Rosado <pabloarosado@gmail.com>
2022-04-25 16:35:18 +0200

    enhance(ingest): remove unused function

--- owid/walden/ingest.py
+++ owid/walden/ingest.py
@@ -1,10 +1,8 @@
 """Tools to ingest to Walden and Catalog."""
 
 from typing import Optional, Union
-from pathlib import Path
 
-from .catalog import Dataset, INDEX_DIR
-from .files import iter_docs
+from .catalog import Dataset
 from .ui import log
 
 
@@ -37,30 +35,3 @@ def add_to_catalog(
     dataset.save()
 
     log("ADDED TO CATALOG", f"{dataset.relative_base}.json")
-
-
-def is_metadata_in_index(namespace, partial_metadata):
-    """Read all metadata files in the walden index, under a certain namespace, and return True if partial_metadata is
-    contained in any of them, with identical values.
-
-    Args:
-        namespace (str): Namespace in walden index.
-        partial_metadata (dict): Fields in metadata to check for matches.
-    """
-    index_dir = Path(INDEX_DIR) / namespace
-    metadata_in_index = False
-    # If namespace is not found, there are obviously no matches.
-    if index_dir.is_dir():
-        for filename, index_file in iter_docs(index_dir):
-            # Iterate over all files in index and check if any of their metadata matches the fields in partial_metadata.
-            dataset_found = True
-            for metadata_field in partial_metadata:
-                dataset_found &= (
-                    index_file.get(metadata_field) == partial_metadata[metadata_field]
-                )
-
-            if dataset_found:
-                log("INFO", f"Metadata already exists in index file: {filename}")
-                metadata_in_index = True
-
-    return metadata_in_index
