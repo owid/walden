cd3f74e2a6ec25326ef57987803a956ad0bbd3bf
Pablo Rosado <pabloarosado@gmail.com>
2022-09-14 16:19:04 +0200

    fix(rff): Store compressed files properly

--- ingests/rff/emissions_weighted_carbon_price__coverage.py
+++ ingests/rff/emissions_weighted_carbon_price__coverage.py
@@ -8,6 +8,8 @@ from owid.walden import Dataset
 
 # Link to download the data.
 DATA_URL = "https://github.com/g-dolphin/ECP/raw/master/_dataset/coverage/tot_coverage_jurisdiction_CO2.csv"
+# Name of output file (which should coincide with this file).
+FILE_NAME = Path(__file__).stem
 
 
 @click.command()
@@ -18,17 +20,18 @@ DATA_URL = "https://github.com/g-dolphin/ECP/raw/master/_dataset/coverage/tot_co
     help="Upload dataset to Walden",
 )
 def main(upload: bool) -> None:
-    metadata = Dataset.from_yaml(Path(__file__).parent / "emissions_weighted_carbon_price__coverage.meta.yml")
+    metadata = Dataset.from_yaml(Path(__file__).parent / f"{FILE_NAME}.meta.yml")
 
     # Read data directly from repository file.
     data = pd.read_csv(DATA_URL)
 
-    with tempfile.NamedTemporaryFile() as temp_file:
+    with tempfile.TemporaryDirectory() as temp_dir:
         # Save data into a temporary file.
-        data.to_csv(temp_file.name + ".zip", index=False, compression="zip")
+        temp_file = Path(temp_dir) / f"{FILE_NAME}.csv.zip"
+        data.to_csv(temp_file, index=False, compression="zip")
 
         # Add the local temporary file to Walden's cache in ~/.owid/walden, with its metadata.
-        dataset = Dataset.copy_and_create(temp_file.name + ".zip", metadata)
+        dataset = Dataset.copy_and_create(str(temp_file), metadata)
 
         # Upload file to S3.
         if upload:
--- ingests/rff/world_carbon_pricing__sources.py
+++ ingests/rff/world_carbon_pricing__sources.py
@@ -8,6 +8,8 @@ from structlog import get_logger
 from owid.walden import Dataset
 
 log = get_logger()
+# Name of output file (which should coincide with this file).
+FILE_NAME = Path(__file__).stem
 
 
 @click.command()
@@ -18,19 +20,20 @@ log = get_logger()
     help="Upload dataset to Walden",
 )
 def main(upload: bool) -> None:
-    metadata = Dataset.from_yaml(Path(__file__).parent / "world_carbon_pricing__sources.meta.yml")
+    metadata = Dataset.from_yaml(Path(__file__).parent / f"{FILE_NAME}.meta.yml")
 
     # Download zipped repository, extract data from files, and concatenated them into one dataframe.
     log.info("Download data.")
     data = extract_data_from_remote_zip_folder(zip_url=WCPD_URL, path_to_folder=WCPD_SOURCES_DIR)
 
-    with tempfile.NamedTemporaryFile() as temp_file:
+    with tempfile.TemporaryDirectory() as temp_dir:
         # Save data into a temporary file.
         log.info("Save data to a temporary file.")
-        data.to_csv(temp_file.name + ".zip", index=False, compression="zip")
+        temp_file = Path(temp_dir) / f"{FILE_NAME}.csv.zip"
+        data.to_csv(temp_file, index=False, compression="zip")
 
         # Add the local temporary file to Walden's cache in ~/.owid/walden, with its metadata.
-        dataset = Dataset.copy_and_create(temp_file.name + ".zip", metadata)
+        dataset = Dataset.copy_and_create(str(temp_file), metadata)
 
         # Upload file to S3.
         if upload:
