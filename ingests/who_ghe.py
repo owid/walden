5c5fb5d547b285168c967bd89615263cbd28e05a
spoonerf <spoonerfiona@gmail.com>
2022-09-30 11:51:53 +0100

    refactor(ghe): removing some unnecessary vars

--- ingests/who_ghe.py
+++ ingests/who_ghe.py
@@ -57,7 +57,7 @@ def download_cause_data(causes) -> pd.DataFrame:
     # Only downloading data for both sex and for all age groups
     for cause in causes:
         log.info("Downloading...", cause=cause)
-        url = f"https://frontdoor-l4uikgap6gz3m.azurefd.net/DEX_CMS/GHE_FULL?$filter=DIM_GHECAUSE_TITLE%20eq%20%27{cause}%27%20and%20DIM_SEX_CODE%20eq%20%27BTSX%27and%20DIM_AGEGROUP_CODE%20eq%20%27ALLAges%27&$select=DIM_GHECAUSE_TITLE,DIM_YEAR_CODE,DIM_COUNTRY_CODE,DIM_AGEGROUP_CODE,DIM_SEX_CODE,VAL_DALY_COUNT_NUMERIC,VAL_DALY_RATE100K_NUMERIC,VAL_DEATHS_COUNT_NUMERIC,VAL_DEATHS_RATE100K_NUMERIC,FLAG_CAUSEGROUP,FLAG_LEVEL,FLAG_SINGLECAUSE"
+        url = f"https://frontdoor-l4uikgap6gz3m.azurefd.net/DEX_CMS/GHE_FULL?$filter=DIM_GHECAUSE_TITLE%20eq%20%27{cause}%27%20and%20DIM_SEX_CODE%20eq%20%27BTSX%27and%20DIM_AGEGROUP_CODE%20eq%20%27ALLAges%27&$select=DIM_GHECAUSE_TITLE,DIM_YEAR_CODE,DIM_COUNTRY_CODE,DIM_AGEGROUP_CODE,DIM_SEX_CODE,VAL_DALY_COUNT_NUMERIC,VAL_DALY_RATE100K_NUMERIC,VAL_DEATHS_COUNT_NUMERIC,VAL_DEATHS_RATE100K_NUMERIC,FLAG_LEVEL"
         df = get_cause_data(url)
         df = repack_frame(df)
         all_data.append(df)
