fcea41b475356ec6d1ebfb8653f29efaa74a7088
spoonerf <spoonerfiona@gmail.com>
2022-10-13 11:45:14 +0100

    refactor(gbd_cm): adding in the probability of death data

--- ingests/ihme_child_mortality.py
+++ ingests/ihme_child_mortality.py
@@ -3,6 +3,7 @@ import os
 import tempfile
 import zipfile
 from pathlib import Path
+from typing import List
 
 import click
 import pandas as pd
@@ -33,17 +34,12 @@ def main(upload: bool) -> None:
 
     with tempfile.TemporaryDirectory() as temp_dir:
         for start_year in start_years:
-            log.info("Downloading data...", year=start_year)
+            log.info("Downloading data...", decade=start_year)
             end_year = start_year + 9
-            url = f"https://ghdx.healthdata.org/sites/default/files/record-attached-files/IHME_GBD_2019_U5M_{start_year}_{end_year}_CT_RT_0.zip"
-            r = requests.get(url)
-            z = zipfile.ZipFile(io.BytesIO(r.content))
-            z.extractall(temp_dir)
-            csv_file = f"IHME_GBD_2019_U5M_{start_year}_{end_year}_CT_RT_Y2021M09D01.CSV"
-            df = pd.read_csv(os.path.join(temp_dir, csv_file))
-            df = df[df["location_id"].isin(locations)]
-            df_all.append(df)
-
+            df_num = get_number_and_rates(start_year, end_year, temp_dir, locations)
+            df_prob = get_probability_death(start_year, end_year, temp_dir, locations)
+            df_all.append(df_num)
+            df_all.append(df_prob)
         dataset = pd.concat(df_all)
         # consolidate data
         dataset = repack_frame(dataset)
@@ -54,7 +50,9 @@ def main(upload: bool) -> None:
 
 
 def get_location_hierachies():
-
+    """
+    Download the location hierachies spreadsheet so that we can filter out the small sub-national regions and the higher level regions we don't want.
+    """
     location_hierachies = "https://ghdx.healthdata.org/sites/default/files/record-attached-files/IHME_GBD_2019_GBD_LOCATION_HIERARCHY_Y2022M06D29.XLSX"
     r = requests.get(location_hierachies).content
     xl = pd.ExcelFile(r)
@@ -63,6 +61,34 @@ def get_location_hierachies():
     lh = lh[lh["Level"].isin([0, 1, 3])]
     locations = lh["Location ID"].to_list()
     return locations
+
+
+def get_number_and_rates(start_year: int, end_year: int, temp_dir: str, locations: List[str]) -> pd.DataFrame:
+    """
+    Download the data on the number and rate of child mortality.
+    """
+    url = f"https://ghdx.healthdata.org/sites/default/files/record-attached-files/IHME_GBD_2019_U5M_{start_year}_{end_year}_CT_RT_0.zip"
+    r = requests.get(url)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(temp_dir)
+    csv_file = f"IHME_GBD_2019_U5M_{start_year}_{end_year}_CT_RT_Y2021M09D01.CSV"
+    df = pd.read_csv(os.path.join(temp_dir, csv_file))
+    df_num = df[df["location_id"].isin(locations)]
+    return df_num
+
+
+def get_probability_death(start_year: int, end_year: int, temp_dir: str, locations: List[str]) -> pd.DataFrame:
+    """
+    Download the data on the probability of death for different age groups of children.
+    """
+    url = f"https://ghdx.healthdata.org/sites/default/files/record-attached-files/IHME_GBD_2019_U5M_{start_year}_{end_year}_POD_0.zip"
+    r = requests.get(url)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(temp_dir)
+    csv_file = f"IHME_GBD_2019_U5M_{start_year}_{end_year}_POD_Y2021M09D01.CSV"
+    df = pd.read_csv(os.path.join(temp_dir, csv_file))
+    df_prob = df[df["location_id"].isin(locations)]
+    return df_prob
 
 
 if __name__ == "__main__":
