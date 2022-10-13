71653d306dc04efafa56f2fe286fd2b182413cd9
Pablo Rosado <pabloarosado@gmail.com>
2022-10-13 16:51:19 +0200

    enhance(ingest): Remove unnecessary code

--- owid/walden/ingest.py
+++ owid/walden/ingest.py
@@ -1,114 +1,16 @@
 """Tools to ingest to Walden and Catalog."""
 
-import inspect
 import tempfile
 from pathlib import Path
-from typing import Any, Optional, Union
+from typing import Optional, Union
 
 import pandas as pd
+from owid.datautils import dataframes
 
 from owid.walden import files
 
 from .catalog import Dataset
 from .ui import log
-
-
-########################################################################################################################
-# TODO: These functions should be removed, and imported from owid.datautils.dataframes once they exist.
-def has_index(df: pd.DataFrame) -> bool:
-    """Return True if a dataframe has an index, and False if it does not (i.e. if it has a dummy index).
-
-    Parameters
-    ----------
-    df : pd.DataFrame
-        Dataframe whose index will be checked.
-
-    Returns
-    -------
-    df_has_index : bool
-        True if dataframe has a non-dummy (single- or multi-) index.
-
-    """
-    # Dataframes always have an attribute index.names, which is a frozen list.
-    # If the dataframe has no set index (i.e. if it has a dummy index), that list contains only [None].
-    # In any other case, the frozen list contains one or more elements different than None.
-    df_has_index = True if df.index.names[0] is not None else False
-
-    return df_has_index
-
-
-def to_file(df: pd.DataFrame, file_path: Union[str, Path], overwrite: bool = True, **kwargs: Any) -> None:
-    """Save dataframe to file.
-
-    This function wraps all pandas df.to_* methods, e.g. df.to_csv() or df.to_parquet(), with the following advantages:
-    * The output file will have the format determined by the extension of file_path. Hence, to_file(df, "data.csv") will
-    create a csv file, and to_file(df, "data.parquet") will create a parquet file.
-    * If file_path is with one or more subfolders that do not exist, the full path will be created.
-    * It can overwrite an existing file (if overwrite is True), or raise an error if the file already exists.
-    * It will avoid creating an index column if the dataframe has a dummy index (which would be equivalent to doing
-    df.to_csv(file_path, index=False)), but it will include the index if the dataframe has one.
-    * Any additional keyword argument that would be passed on to the method to write a file can be safely added. For
-    example, to_file(df, "data.csv", na_rep="TEST") will replace missing data by "TEST" (analogous to
-    df.to_csv("data.csv", na_rep="TEST")).
-
-    Parameters
-    ----------
-    df : pd.DataFrame
-        Dataframe to be stored in a file.
-    file_path : Union[str, Path]
-        Path to file to be created.
-    overwrite : bool, optional
-        True to overwrite file if it already exists. False to raise an error if file already exists.
-
-    """
-    # Ensure file_path is a Path object.
-    file_path = Path(file_path)
-
-    # Ensure extension is lower case and does not start with '.'.
-    extension = file_path.suffix.lstrip(".").lower()
-
-    # Ensure output directory exists.
-    if not file_path.parent.exists():
-        file_path.parent.mkdir(parents=True)
-
-    # Avoid overwriting an existing file unless explicitly stated.
-    if file_path.is_file() and not overwrite:
-        raise FileExistsError("Failed to save dataframe because file exists and 'overwrite' is False.")
-
-    # Available output methods (some of them may need additional dependencies to work).
-    output_methods = {
-        "csv": df.to_csv,
-        "dta": df.to_stata,
-        "feather": df.to_feather,
-        "hdf": df.to_hdf,
-        "html": df.to_html,
-        "json": df.to_json,
-        "md": df.to_markdown,
-        "parquet": df.to_parquet,
-        "pickle": df.to_pickle,
-        "pkl": df.to_pickle,
-        "tex": df.to_latex,
-        "txt": df.to_string,
-        "xlsx": df.to_excel,
-        "xml": df.to_xml,
-    }
-    if extension not in output_methods:
-        raise ValueError(f"Failed saving dataframe because of an unknown file extension: {extension}")
-    # Select the appropriate storing method.
-    save_function = output_methods[extension]
-
-    # Decide whether dataframe should be stored with or without an index, if:
-    # * The storing method allows for an 'index' argument.
-    # * The argument "index" is not explicitly given.
-    if ("index" in inspect.signature(save_function).parameters) and ("index" not in kwargs):
-        # Make 'index' False to avoid storing index if dataframe has a dummy index.
-        kwargs["index"] = has_index(df=df)
-
-    # Save file using the chosen save function and the appropriate arguments.
-    save_function(file_path, **kwargs)
-
-
-########################################################################################################################
 
 
 def add_to_catalog(
@@ -153,7 +55,7 @@ def add_to_catalog(
             # Save dataframe in a temporary file.
             # Use the extension specified in the metadata, so that the file is stored in the right format.
             temp_file = Path(temp_dir) / f"temp.{file_extension}"
-            to_file(dataframe, file_path=temp_file)
+            dataframes.to_file(dataframe, file_path=temp_file)
             # Add file checksum to metadata.
             metadata.md5 = files.checksum(temp_file)  # type: ignore
             # Run the function again, but now fetching the data from the temporary file instead of the dataframe.
