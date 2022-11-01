85ede72bee76cdc6ff0f2a7df32f59888eda544b
lucasrodes <lucasrodes@users.noreply.github.com>
2022-11-01 16:02:57 +0100

    enhance(riley2005): use upload_and_save

--- ingests/papers/2022-11-01/riley2005/__main__.py
+++ ingests/papers/2022-11-01/riley2005/__main__.py
@@ -12,13 +12,12 @@ To find which code generated your dataset version "YYYY-MM-DD", look for the cod
 backwards.
 """
 
-from datetime import datetime
 from pathlib import Path
 
 import click
 from structlog import get_logger
 
-from owid.walden import Catalog, Dataset
+from owid.walden import Dataset
 
 log = get_logger()
 
@@ -39,45 +38,11 @@ def main(upload: bool) -> None:
     # download dataset from source_data_url and add the local file to Walden's cache in ~/.owid/walden
     dataset = Dataset.download_and_create(metadata)
 
-    if needs_to_be_updated(dataset):
-        log.info("Update needed! Updating dataset...")
-        # Update version, publication year
-        dataset.version = dataset.date_accessed
+    # Update version
+    dataset.version = dataset.date_accessed
 
-        # upload it to S3
-        if upload:
-            dataset.upload(public=True)
-
-        # update PUBLIC walden index with metadata
-        dataset.save()
-    else:
-        log.info("No update needed!")
-
-
-def needs_to_be_updated(dataset: Dataset) -> bool:
-    """Check if dataset needs to be updated.
-
-    Retrieves last version of the dataset in Walden and compares it to the current version. Comparison is done by
-    string comparing the MD5 checksums of the two datasets.
-
-    TODO: move to class method of Dataset.
-
-    Parameters
-    ----------
-    dataset : Dataset
-        Dataset that was just retrieved.
-
-    Returns
-    -------
-    bool
-        True if dataset in Walden is outdated and a new version is needed.
-    """
-    log.info("Checking if dataset needs to be updated...")
-    try:
-        dataset_last = Catalog().find_latest(namespace=dataset.namespace, short_name=dataset.short_name)
-        return dataset_last.md5 != dataset.md5
-    except ValueError:
-        return True
+    # Upload dataset
+    dataset.upload_and_save(upload=upload, public=True)
 
 
 if __name__ == "__main__":
