1f4a0022e09d35d6270e9291935e7296f68622a0
Pablo Rosado <pabloarosado@gmail.com>
2022-11-03 16:18:47 +0100

    style(gcp): Improve format

--- ingests/gcp/2022-11-11/global_carbon_budget.py
+++ ingests/gcp/2022-11-11/global_carbon_budget.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 
 import click
+
 from owid.walden import Dataset
 
 # TODO: Change to public on November 11 (date of public release).
@@ -13,9 +14,11 @@ CURRENT_DIR = Path(__file__).parent
 # List of metadata files with the information (including download URLs) of data files to download.
 METADATA_FILES = [
     # Fossil CO2 emissions dataset (long csv file), containing global and national data on fossil fuel CO2 emissions
-    # from 1750 until today..
-    CURRENT_DIR / "global_carbon_budget_fossil_co2_emissions.meta.yml",
+    # from 1750 until today.
+    CURRENT_DIR
+    / "global_carbon_budget_fossil_co2_emissions.meta.yml",
 ]
+
 
 @click.command()
 @click.option(
